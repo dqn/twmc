1182ceb0cea496fae0ccdffceb21d2025e50a07f
dqn <suyarizzz@gmail.com>
2019-09-23 18:30:58 +0900

    feat: download media

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"path"
+	"strings"
 	"syscall"
 
 	"github.com/BurntSushi/toml"
@@ -13,7 +17,7 @@ import (
 )
 
 type Config struct {
-	Authentication Authentication
+	Authentication     Authentication
 	StreamFilterParams twitter.StreamFilterParams
 }
 
@@ -22,6 +26,26 @@ type Authentication struct {
 	ConsumerSecret string
 	AccessToken    string
 	AccessSecret   string
+}
+
+func download(url string) {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	_, filename := path.Split(url)
+	out, err := os.Create(fmt.Sprintf("./media/%v", filename))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
@@ -47,7 +71,31 @@ func main() {
 
 	demux := twitter.NewSwitchDemux()
 	demux.Tweet = func(tweet *twitter.Tweet) {
-		fmt.Println(tweet.Text)
+		media := func() []twitter.MediaEntity {
+			if tweet.ExtendedEntities != nil {
+				return tweet.ExtendedEntities.Media
+			}
+			if tweet.Entities != nil {
+				return tweet.Entities.Media
+			}
+			return nil
+		}()
+
+		for _, medium := range media {
+			if variants := medium.VideoInfo.Variants; len(variants) > 0 {
+				// 動画
+				for _, variant := range variants {
+					if variant.ContentType == "video/mp4" {
+						download(strings.Split(variant.URL, "?")[0])
+						fmt.Println(variant.URL)
+					}
+				}
+			} else {
+				// 画像
+				download(medium.MediaURLHttps)
+				fmt.Println(medium.MediaURLHttps)
+			}
+		}
 	}
 
 	stream, err := client.Streams.Filter(&config.StreamFilterParams)
